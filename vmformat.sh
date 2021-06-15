a78bb97eabff2a55b9d785b2711e32bfb99686af
david-descherer1 <david.descherer@gmail.com>
2021-06-15 11:46:39 -0400

    basic VM add

--- vmformat.sh
+++ vmformat.sh
@@ -16,4 +16,28 @@ subscriptionID=$(az account show --query id --output tsv)
 
 az group create -n $imageResourceGroup -l $location
 
-az identity create -g $imageResourceGroup -n <myUserAssignedIdentity>
+identityName=aibBuiUserId$(date +'%s')
+az identity create -g $imageResourceGroup -n $identityName
+
+imgBuilderCliId=$(az identity show -g $imageResourceGroup -n $identityName | grep "clientId" | cut -c16- | tr -d '",')
+
+imgBuilderId=/subscriptions/$subscriptionID/resourcegroups/$imageResourceGroup/providers/Microsoft.ManagedIdentity/userAssignedIdentities/$identityName
+
+curl https://raw.githubusercontent.com/azure/azvmimagebuilder/master/solutions/12_Creating_AIB_Security_Roles/aibRoleImageCreation.json -o aibRoleImageCreation.json
+
+imageRoleDefName="Azure Image Builder Image Def"$(date +'%s')
+
+sed -i -e "s/<subscriptionID>/$subscriptionID/g" aibRoleImageCreation.json
+sed -i -e "s/<rgName>/$imageResourceGroup/g" aibRoleImageCreation.json
+sed -i -e "s/Azure Image Builder Service Image Creation Role/$imageRoleDefName/g" aibRoleImageCreation.json
+
+# create role definitions
+az role definition create --role-definition ./aibRoleImageCreation.json
+
+az role assignment create \
+    --assignee $imgBuilderCliId \
+    --role Contributor \
+    --scope /subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup
+
+
+
