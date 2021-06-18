bc9289af4c8e49188c2bc895d1198528e5f8d99b
david-descherer1 <david.descherer@gmail.com>
2021-06-18 11:50:38 -0400

    deletion

--- anacondaDownload.ps1
+++ anacondaDownload.ps1
@@ -1,4 +1,3 @@
 #Script to Install Anaconda3 on Windows without UI
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -Uri "https://repo.anaconda.com/archive/Anaconda3-2021.05-Windows-x86.exe" -OutFile "c:/temp/Anaconda3-2021.05-Windows-x86.exe"
-start /wait "" Anaconda3-2021.05-Windows-x86.exe /InstallationType=JustMe /S /D=c:/temp/Anaconda3-2021.05-Windows-x86.exe
