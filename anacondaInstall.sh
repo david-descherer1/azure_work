4d141e21aaa6ed7d52fc67cc270b67203090df0d
david-descherer1 <david.descherer@gmail.com>
2021-06-18 17:18:23 -0400

    anaconda install edits

--- anacondaInstall.sh
+++ anacondaInstall.sh
@@ -1,3 +1,5 @@
 #Script to finish install of Anaconda
 #anaconda download script must precede this script
 start /wait "" Anaconda3-2021.05-Windows-x86.exe /InstallationType=JustMe /AddToPath=1 /S /D=c:/temp/Anaconda3-2021.05-Windows-x86.exe
+#line below is for testing purposes
+#start /wait "" Anaconda3-2021.05-Windows-x86.exe /InstallationType=JustMe /AddToPath=1 /S /D=c:/temp/Anaconda3-2021.05-Windows-x86.exe
