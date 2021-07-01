069ad3e3706175b8e26c3d4f4447f1d102fd45ac
david-descherer1 <david.descherer@gmail.com>
2021-07-01 13:18:47 -0400

    upload dr probe gui

--- installDrProbeGUI.ps1
+++ installDrProbeGUI.ps1
@@ -1,5 +1,5 @@
 #Script to install Dr. Probe GUI
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri "https://www.dropbox.com/s/ml534wztyxeottk/InstallDrProbe_1_10_1.zip?dl=0" -OutFile "c:/temp/InstallDrProbe_1_10_1.zip"
+Invoke-WebRequest -Uri "https://er-c.org/barthel/drprobe/InstallDrProbe_1_10_1.zip" -OutFile "c:/temp/InstallDrProbe_1_10_1.zip"
 Expand-Archive "c:\\temp\\InstallDrProbe_1_10_1.zip" "c:\\temp"
 & "c:\\temp\\InstallDrProbe.exe" /run /exit /SilentMode
